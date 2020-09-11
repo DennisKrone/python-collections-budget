a7cdc070f36091b0cc1c12aa3daa02e01f67c345
Dennis Kroné <dennis.krone@svenskaspel.se>
2020-09-11 14:02:44 +0200

    fix parameter errors

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,11 +17,10 @@ def main():
 
     print(
         timeit.timeit(
-            stmt=expenses.categorize_for_loop(),
-            setup="""
-        from . import Expense
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')
+            stmt="expenses.categorize_for_loop()",
+            setup="""from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
         """,
             number=100000,
             globals=globals(),
@@ -30,11 +29,10 @@ def main():
 
     print(
         timeit.timeit(
-            stmt=expenses.categorize_set_comprehension(),
-            setup="""
-        from . import Expense
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')
+            stmt="expenses.categorize_set_comprehension()",
+            setup="""from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
         """,
             number=100000,
             globals=globals(),
